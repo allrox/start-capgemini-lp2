a9669caf942355b1fbb0f50d0c849204961891b4
allrox <alexandre@allrox.com.br>
2023-08-08 20:25:24 -0300

    Estudos sobre o método Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,31 @@
+from random import randrange
+
+# Definindo uma lista para teste
+lista = []
+
+def elementosAleatorios(y):
+    for x in range(y):
+        lista.append(randrange(0,1000))
+
+
+elementosAleatorios(3)
+print(f"Lista com os elementos na ordem original {lista}")
+
+# Captura o tamanho da lista
+n = len(lista)
+
+# Laço de comparação
+for i in range(n):
+
+    # Para cada índice no range de 0 a n-i-1 Desta forma, a cada passagem o loop interno
+    # reduz o número de comparações desnecessárias, já que o maior elemento SEMPRE flutua
+    # para o final da lista na primeira passagem. Assim, na repetições do laço principal
+    # pode-se subtrair um elemento sem prejuízo da função
+    for j in range(0, n-i-1):
+        # Se o item do índice J for maior que o elemento do próximo índice
+        if lista[j] > lista[j+1]:
+            # Os elementos trocam de posição em seus índices
+            lista[j], lista[j+1] = lista[j+1], lista[j]
+
+
+print(f"Lista com elementos reordenados {lista}")
